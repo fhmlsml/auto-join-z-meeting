2ff0b001e358cd41226683eb5c65bd93f6fcdd64
Fhmisml <38683083+fhmlsml@users.noreply.github.com>
2021-07-10 05:43:02 +0700

    dikit ae

--- join_z_meeting.py
+++ join_z_meeting.py
@@ -6,14 +6,14 @@ import time
 
 url = 'https://your-z-meeting-url.com' # Change with your z-meeting url
 
-chrome_path = '/usr/bin/google-chrome' # This path is from linux ubuntu, change your own path
+chrome_path = '/usr/bin/google-chrome' # This path is from linux ubuntu, change your own chrome path
 
 # One Time job
 def job():
     webbrowser.get(chrome_path).open(url) # Do some work that only needs to happen once...
     return schedule.CancelJob
 
-schedule.every().day.at('15:00').do(job) # Change your desired time
+schedule.every().day.at('15:00').do(job) # Change your meeting time
 
 print("Waiting for the time to join . . .")
 
